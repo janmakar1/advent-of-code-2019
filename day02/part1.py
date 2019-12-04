9d6c129b9f6b4375f87264402d8ab7b2b89ed1cb
Jan Kiel <jan.kiel665@gmail.com>
2019-12-05 00:36:22 +0100

    Just some refactoring in day 2 part 1

--- day02/part1.py
+++ day02/part1.py
@@ -16,30 +16,41 @@ def processCommand(opcode, idx_of_operand1, idx_of_operand2, data):
     return result
 
 
-input = open("input.txt", "r")
-program_code = input.readline()
-program_code = program_code.rstrip()
-dataStr = program_code.split(",")
-data = [int(d) for d in dataStr]
+def mainLoop(data):
+    for i in range(0, len(data), 4):
+        # print(i, data[i])
+        if(int(data[i]) == 99):
+            break
+        else:
+            result = processCommand(int(data[i]), int(data[i+1]), 
+            int(data[i+2]), data)
+            # print(f"will save result {result} into index {data[i+3]}")
+            data[data[i+3]] = result
+
+    print(data)
+    print(f">> Answer is: {data[0]}") # answer: 2692315 
+
+    dataStr = [str(d) for d in data]
+    output = ",".join(dataStr)
+    print(output)
+
+
+def parseFile(filename):
+    input = open(filename, "r")
+    program_code = input.readline()
+    program_code = program_code.rstrip()
+    dataStr = program_code.split(",")
+    data = [int(d) for d in dataStr]
+    input.close() # do i need to do this?
+    return data
+
+
+
+data = parseFile("input.txt")
 
 # 1202 procedure
 data[1] = 12
 data[2] = 2
 
 # MAIN LOOP
-for i in range(0, len(data), 4):
-    # print(i, data[i])
-    if(int(data[i]) == 99):
-        break
-    else:
-        result = processCommand(int(data[i]), int(data[i+1]), 
-        int(data[i+2]), data)
-        # print(f"will save result {result} into index {data[i+3]}")
-        data[data[i+3]] = result
-
-print(data)
-print(f"Answer is: {data[0]}")
-
-dataStr = [str(d) for d in data]
-output = ",".join(dataStr)
-print(output)
+mainLoop(data)
